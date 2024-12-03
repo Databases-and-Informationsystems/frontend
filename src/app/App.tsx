0616b4d3a26d07315bfbc916c907249ec046a4ee
Constructor44 <fmshd29@gmail.com>
2024-12-03 14:48:46 +0100

    Set page to use full height and width; Initial navigation menu added (<NavigationHeader/>), taking the project name as a parameter

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,10 +3,12 @@ import '../App.css'
 import { AppProvider } from './provider'
 import { Button } from '@/components/ui/button'
 import { ModeToggle } from '@/components/ThemeToggle/ThemeToggle'
+import { NavigationHeader } from '@/features/annotation_tool/components/navigation-header.tsx'
 
 function App() {
   return (
     <AppProvider>
+      <NavigationHeader project_name={"project_name"} />
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
         Testing
       </h1>
