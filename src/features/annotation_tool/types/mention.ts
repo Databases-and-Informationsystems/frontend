16a6818baa9f507abddb048d2234c79776dad3ed
JannicMeetYu <j.herrmann@meetyu.app>
2024-12-09 11:15:53 +0100

    Created token and mention type

---
+++ src/features/annotation_tool/types/mention.ts
@@ -0,0 +1,6 @@
+export type Mention = {
+  id: string;
+  tag: string; 
+  isShownRecommendation: boolean;
+  token_ids: string[];
+}
---
+++ src/features/annotation_tool/types/token.ts
@@ -0,0 +1,8 @@
+export type Token = {
+  id: string;
+  text: string;
+  index_in_document: number;
+  pos_tag: string;      
+  bio_tag: string;
+  sentence_index: number;
+}
