e804d336d33965c5b64ee57f92b50a092820df5f
Constructor44 <fmshd29@gmail.com>
2024-12-09 13:32:09 +0100

    Added navigation header logic basics

--- src/features/annotation_tool/components/navigation-header.tsx
+++ src/features/annotation_tool/components/navigation-header.tsx
@@ -1,7 +1,35 @@
 import { NavigationMenu, NavigationMenuItem, NavigationMenuLink } from '@/components/ui/navigation-menu.tsx'
+import { useEffect, useState } from 'react'
+import { AnnotationStates } from '@/features/annotation_tool/components/annotation-states.tsx'
 
 
 export function NavigationHeader(props: {project_name: string}) {
+
+  const [currentState, setCurrentState] = useState(AnnotationStates.NotStarted);
+  const [changeToState, setChangeToState] = useState(AnnotationStates.NoChange);
+
+  useEffect(() => {
+    if (changeToState !== AnnotationStates.NoChange) {
+      //logic to change save and set the currentState
+      setCurrentState(changeToState); //debug mode :)
+      setChangeToState(AnnotationStates.NoChange);
+    }
+  })
+
+  function SplitOptions() {
+    if (currentState === AnnotationStates.RelationEditing) {
+      return (
+        <>
+          <NavigationMenuItem>
+            <NavigationMenuLink onClick={() => {/*TODO*/}} style={{cursor: "pointer"}} className="text-lg font-medium">
+              split view
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+        </>
+      )
+    }
+  }
+
   return (
     <div className="top-0">
       <h2 className="text-left text-3xl font-bold">Annotating: <span className="italic">{props.project_name}</span></h2>
@@ -9,27 +37,27 @@ export function NavigationHeader(props: {project_name: string}) {
         <NavigationMenu>
           <div className="flex space-x-4">
             <NavigationMenuItem>
-              <NavigationMenuLink href="/m_sug" className="text-lg font-medium">
+              <NavigationMenuLink onClick={() => setChangeToState(AnnotationStates.MentionSuggestion)} style={{cursor: "pointer"}} className="text-lg font-medium">
                 Mention suggestion
               </NavigationMenuLink>
             </NavigationMenuItem>
             <NavigationMenuItem>
-              <NavigationMenuLink href="/m_edt" className="text-lg font-medium">
+              <NavigationMenuLink onClick={() => setChangeToState(AnnotationStates.MentionEditing)} style={{cursor: "pointer"}} className="text-lg font-medium">
                 Mention editing
               </NavigationMenuLink>
             </NavigationMenuItem>
             <NavigationMenuItem>
-              <NavigationMenuLink href="/e_sel" className="text-lg font-medium">
+              <NavigationMenuLink onClick={() => setChangeToState(AnnotationStates.EntitySelection)} style={{cursor: "pointer"}} className="text-lg font-medium">
                 Entity selection
               </NavigationMenuLink>
             </NavigationMenuItem>
             <NavigationMenuItem>
-              <NavigationMenuLink href="/r_sug" className="text-lg font-medium">
+              <NavigationMenuLink onClick={() => setChangeToState(AnnotationStates.RelationSuggestion)} style={{cursor: "pointer"}} className="text-lg font-medium">
                 Relation suggestion
               </NavigationMenuLink>
             </NavigationMenuItem>
             <NavigationMenuItem>
-              <NavigationMenuLink href="/r_edt" className="text-lg font-medium">
+              <NavigationMenuLink onClick={() => setChangeToState(AnnotationStates.RelationEditing)} style={{cursor: "pointer"}} className="text-lg font-medium">
                 Relation editing
               </NavigationMenuLink>
             </NavigationMenuItem>
@@ -37,21 +65,29 @@ export function NavigationHeader(props: {project_name: string}) {
         </NavigationMenu>
       </div>
       <NavigationMenu>
-        <NavigationMenuItem>
-          <NavigationMenuLink onClick={() => {/*TODO*/}} className="text-lg font-medium">
-            u
-          </NavigationMenuLink>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuLink onClick={() => {/*TODO*/}} className="text-lg font-medium">
-            f
-          </NavigationMenuLink>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuLink onClick={() => {/*TODO*/}} className="text-lg font-medium">
-            save
-          </NavigationMenuLink>
-        </NavigationMenuItem>
+        <div className="flex space-x-4">
+          <NavigationMenuItem>
+            <NavigationMenuLink onClick={() => {/*TODO*/}} style={{cursor: "pointer"}} className="text-lg font-medium">
+              u
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuLink onClick={() => {/*TODO*/}} style={{cursor: "pointer"}} className="text-lg font-medium">
+              f
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuLink onClick={() => {/*TODO*/}} style={{cursor: "pointer"}} className="text-lg font-medium">
+              save
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuLink onClick={() => {/*TODO*/}} style={{cursor: "pointer"}} className="text-lg font-medium">
+              download
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+          <SplitOptions></SplitOptions>
+        </div>
       </NavigationMenu>
     </div>
   )
