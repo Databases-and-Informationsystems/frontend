6a46e5aa008c4c9ec8f7b6046fab40221884b770
Rahma Jendoubi <rahma.jendoubi@uni-bayreuth.de>
2024-12-11 03:56:15 +0100

    switching between the Login and settings pages

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+/*import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
 import Sidebar from "./Sidebar"; 
 import Settings from "./Settings"; 
 import './index.css'; 
@@ -12,4 +14,34 @@ const App: React.FC = () => {
   );
 };
 
+export default App;*/
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Sidebar from "../components/Sidebar";
+import Settings from "../components/Settings";
+import Login from "../components/Login";
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        {/* Route for Login */}
+        <Route path="/" element={<Login />} />
+
+        {/* Route for Settings with Sidebar */}
+        <Route
+          path="/settings"
+          element={
+            <div style={{ display: "flex", height: "100vh" }}>
+              <Sidebar />
+              <Settings />
+            </div>
+          }
+        />
+      </Routes>
+    </Router>
+  );
+};
+
 export default App;
+
