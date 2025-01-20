ea0165ac6df1884128682529951505840de6f556
JannicMeetYu <j.herrmann@meetyu.app>
2025-01-20 11:33:59 +0100

    Added protected routes

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,27 +1,32 @@
 import DashboardLayout from '@/features/dashboard/components/DashboardLayout';
-import { Routes, Route } from 'react-router';
+import { Routes, Route, Navigate } from 'react-router';
 import Dashboard from '@/features/dashboard/components/Dashboard';
 import ProjectPage from '@/features/projects/components/ProjectPage';
 import SchemaPage from '@/features/schema/SchemaPage';
 import Login from '@/features/login/pages/Login';
 import Teams from '@/features/Teams/Teams';
 import Settings from '@/features/settings/Settings';
+import { ProtectedRoute } from '@/components/ProtectedRoute';
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/login" element={<Login/>} />
-      
-      <Route path="/dashboard" element={<DashboardLayout />}>
-        <Route index element={<Dashboard />} />
-        <Route path="teams" element={<Teams/>} />
-        
-        <Route path="projects" element={<ProjectPage />} />
-        <Route path="schemas" element={<SchemaPage/>} />
-        <Route path="projects/settings" element={<Settings/>} />
-      </Route>
+      <Route path="/" element={<Navigate to="/login" replace />} />
 
+      <Route path="/login" element={<Login />} />
+      <Route element={<ProtectedRoute />}>
+        <Route path="/dashboard" element={<DashboardLayout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="teams" element={<Teams />} />
+
+          <Route path="projects" element={<ProjectPage />} />
+          <Route path="schemas" element={<SchemaPage />} />
+          <Route path="projects/settings" element={<Settings />} />
+        </Route>
+      </Route>
       <Route path="/annotation" element={<div>Annotation</div>} />
+
+      <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
   );
 };
