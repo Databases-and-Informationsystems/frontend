092d9ce2e3ba8a3ccb579283a8f175fe7254fdca
JannicMeetYu <j.herrmann@meetyu.app>
2025-01-27 13:52:10 +0100

    Adjusted schema and token api

--- src/features/annotation_tool/api/schema.ts
+++ src/features/annotation_tool/api/schema.ts
@@ -1,14 +1,7 @@
-import axios from "axios";
+import axiosInstance from "@/lib/axios";
 import { Schema } from "../types";
 
-const BASE_URL = 'http://localhost:3000/schema'
-
-export const fetchSchema = async (): Promise<Schema> => {
-  try {
-    const response = await axios.get(BASE_URL)
-    return response.data
-  } catch (error) {
-    console.error('Failed to fetch schema:', error)
-    throw error
-  }
+export const fetchSchema = async (schemaId: string): Promise<Schema> => {
+  const response = await axiosInstance.get(`/schema/${schemaId}`);
+  return response.data.schema;
 }
