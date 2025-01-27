70aefff1392e796194ee4fa5689ec0f13055109e
JannicMeetYu <j.herrmann@meetyu.app>
2025-01-27 14:07:34 +0100

    Adjusted mention and relation type

--- src/features/annotation_tool/types/mention.ts
+++ src/features/annotation_tool/types/mention.ts
@@ -1,7 +1,12 @@
-// TODO: Change IDs to number later on
+import { SchemaMention } from "@/features/schema/types/types";
+import { Token } from "./token";
+
 export type Mention = {
-  id: string;
+  id: number;
   tag: string; 
   isShownRecommendation: boolean;
-  token_ids: string[];
+  document_edit_id: number;
+  document_recommendation_id: number;
+  tokens: Token[];
+  schema_mention: SchemaMention;
 }
--- src/features/annotation_tool/types/relation.ts
+++ src/features/annotation_tool/types/relation.ts
@@ -1,8 +1,26 @@
+import { SchemaRelation } from "@/features/schema/types/types";
+import { Token } from "./token";
+
 export type Relation = {
-    id: string;
-    tag: string;
+    id: number;
     isDirected: boolean;
+    tag: string;
     isShownRecommendation: boolean;
-    mention_head_id: string;
-    mention_tail_id: string;
+    head_mention: HeadMention;
+    tail_mention: TailMention;
+    schema_relation: SchemaRelation;
+    document_edit_id: number;
+    document_recommendation_id: number;
+}
+
+export type HeadMention = {
+    tag: string;
+    tokens: Token[];
+    entity: number;
+}
+
+export type TailMention = {
+    tag: string;
+    tokens: Token[];
+    entity: number;
 }
