41a23c671b0f2c9fa078ee2abd53f25f72655a3e
Constructor44 <fmshd29@gmail.com>
2025-02-02 20:19:07 +0100

    Made the entities fit to the backend ones

--- src/features/annotation_tool/types/entity.ts
+++ src/features/annotation_tool/types/entity.ts
@@ -1,4 +1,9 @@
+import { Mention } from '@/features/annotation_tool/types/mention.ts'
+
 export type AnnotationEntity = {
   id: number;
-  mention_ids: Array<number | string>;
+  isShownRecommendation: boolean;
+  document_edit_id: number;
+  document_recommendation_id: number;
+  mentions: Array<Mention>;
 }
