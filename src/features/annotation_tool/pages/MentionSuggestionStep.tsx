4e182e86545aa3e2f92d5636a954131d211649b8
JannicMeetYu <j.herrmann@meetyu.app>
2025-02-17 19:36:37 +0100

    fix: added loading UI

--- src/features/annotation_tool/pages/MentionSuggestionStep.tsx
+++ src/features/annotation_tool/pages/MentionSuggestionStep.tsx
@@ -1,18 +1,49 @@
+import { useEffect } from 'react';
 import { AnnotatedText } from '../components/AnnotatedText';
 import { useMentionContext } from '../context/useMentionContext';
+import { workflowOrder, WorkflowStep } from '../types/workflow';
+import { toast } from 'sonner';
+import { useWorkflowContext } from '../context/useWorkflowContext';
+import { TriangleAlert } from 'lucide-react';
 
 export const MentionSuggestionStep = () => {
   const { mentions } = useMentionContext();
+  const { maxStep, updateStep, updateMaxStep } = useWorkflowContext();
+
 
   const hasSuggestions = mentions.some(mention => mention.isShownRecommendation === true);
 
-  if (!hasSuggestions) {
-    return <p>No suggestions available. This step is complete.</p>;
-  }
+  const nextStep: WorkflowStep = 'MENTIONS';
+
+  useEffect(() => {
+    if (!hasSuggestions) {
+      const isForward = isForwardStep(nextStep, maxStep);
+      if (isForward) {
+        updateMaxStep(nextStep).then(success => {
+          if (!success) {
+            toast.warning('Failed to update step.', {
+              className: 'text-base',
+              icon: <TriangleAlert />,
+            });
+            return;
+          }
+          updateStep(nextStep);
+        });
+      } else {
+        updateStep(nextStep);
+      }
+    }
+  }, [hasSuggestions, maxStep, nextStep, updateMaxStep, updateStep]);
 
   return (
     <>
       <AnnotatedText showDeleteButton={false} />
     </>
   )
+}
+
+function isForwardStep(targetStep: WorkflowStep, currentMax: WorkflowStep): boolean {
+  const currentIndex = workflowOrder.indexOf(currentMax);
+  const targetIndex = workflowOrder.indexOf(targetStep);
+  return targetIndex > currentIndex;
 }
